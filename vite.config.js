3e0de9479612e7d34b55e1f4982438ba909c3a41
tejas-tj29 <btech10225.25@bitmesra.ac.in>
2026-06-04 18:22:43 +0530

    fix: update import statement for React plugin in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/react-plugin';
+import react from '@vitejs/plugin-react'; // <-- Notice the name change here!
 
 export default defineConfig({
   plugins: [react()],
-  // This dot makes asset paths relative (./), fixing BOTH Chrome Extensions and GitHub Pages!
   base: './', 
 });
